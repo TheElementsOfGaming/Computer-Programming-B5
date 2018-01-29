cd141f1b8acd6be64cc253059a7128176b9665ae
Kyler Draper <kythegamedeveloper@gmail.com>
2018-01-28 21:51:31 -0700

    Finished DoItAgain

--- DoItAgain/DoItAgain/Program.cs
+++ DoItAgain/DoItAgain/Program.cs
@@ -6,7 +6,84 @@ namespace DoItAgain
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+               displayMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Print Numbers");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1" || result == "Print Numbers")
+            {
+                PrintNumbers();
+                return true;
+            }
+            else if (result == "2" || result == "Guessing Game")
+            {
+                GuessingGame();
+                return true;
+            }
+            else if (result == "3" || result == "Exit")
+            {
+                return false; 
+            }
+            else 
+            {
+                return true; 
+            }
+
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print numbers!");
+            Console.WriteLine("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guessing game!");
+
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+
+
+            Console.ReadLine();
         }
     }
 }
