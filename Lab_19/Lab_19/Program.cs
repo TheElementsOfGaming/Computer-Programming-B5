25a9389b182f975ecc728cc0a78ab2787ec7e62c
Kyler Draper <32275825+TheElementsOfGaming@users.noreply.github.com>
2018-01-31 14:27:34 -0700

    Added Lab_19

---
+++ Lab_19/Lab_19/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lab_19
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //create a random number generator
+            Random rng = new Random();
+            //create a string named response
+            string response;
+            //create a do loop for the whole program.
+            do
+            {
+                //do loop to validate the user’s response of yes(y) or no(n)
+                do
+                {
+
+
+                    //Ask the user if they want to roll the dice
+                    Console.WriteLine("Do you want to roll the dice?");
+                    //Get the users input 
+                    response = Console.ReadLine();
+
+
+                } while (response != "y" && response != "n");
+
+                //if the user answered “yes(y) roll the dice 
+                if (response == "y")
+                {
+                    //declare two integers die1 and die2
+                    int die1, die2;
+                    //set the value of die1 to random number between 1 and 6
+                    die1 = rng.Next(1, 7);
+                    //set the value of die2 to random number between 1 and 6 
+                    die2 = rng.Next(1, 7);
+                    //if die 1 == die2 == 6 
+                    if (die1 == 6 && die2 == 6)
+                    {
+                        //print out you rolled boxcars
+                        Console.WriteLine("You rolled boxcars!");
+                    }
+                    //else if die 1 == die 2 == 1
+                    else if (die1 == 1 && die2 == 1)
+                    {
+
+                        //print out you rolled snake eyes
+                        Console.WriteLine("You rolled snake eyes!");
+                    }
+                    //else
+                    else
+                        //print out you rolled a 5 and 6, replacing the 5 and 6 wilth the real roll values
+                        Console.WriteLine("You rolled {0} and a {1}", die1, die2);
+                }
+            } while (response != "n");
+
+        }
+    }
+}
