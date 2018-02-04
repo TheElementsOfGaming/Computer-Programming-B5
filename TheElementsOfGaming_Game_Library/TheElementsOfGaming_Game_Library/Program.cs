0498a446d784f99fc16b6a6206ac284096e3eda5
Kyler Draper <kythegamedeveloper@gmail.com>
2018-02-04 14:20:47 -0700

    Added Guessing Game to Game Library

--- TheElementsOfGaming_Game_Library/TheElementsOfGaming_Game_Library/Program.cs
+++ TheElementsOfGaming_Game_Library/TheElementsOfGaming_Game_Library/Program.cs
@@ -13,17 +13,24 @@ namespace TheElementsOfGaming_Game_Library
             Console.Clear();
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Dice Game");
-            Console.WriteLine("2) Exit");
+            Console.WriteLine("2) Guessing Game");
+            Console.WriteLine("3) Exit");
             string output = Console.ReadLine();
             if (output == "1" || output == "Dice Game")
             {
                 Console.Clear();
                 Dice_Game();
             }
-            else if (output == "2" || output == "Exit")
+            else if (output == "2" || output == "Guessing Game")
             {
-                Environment.Exit(1);
+                Console.Clear();
+                guessing_Game();
             }
+            else if (output == "3" || output == "Exit")
+            {
+                Environment.Exit(0);
+            }
+
         }
         private static void Dice_Game()
         {
@@ -103,7 +110,44 @@ namespace TheElementsOfGaming_Game_Library
             } while (response != "n");
 
         }
+        private static void guessing_Game()
+        {
+            Console.Clear();
+            Console.WriteLine("Would you like to play the guessing game?");
+            string input = Console.ReadLine();
+            if (input == "y" || input == "yes")
+            {
+                
+            Console.WriteLine("Guessing game!");
 
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+
+            } while (incorrect);
+            Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+                Console.ReadLine();
+                guessing_Game();
+            }
+            else
+            {
+                Start();
+            }
+        
+        }
+            
     }
 
 }
