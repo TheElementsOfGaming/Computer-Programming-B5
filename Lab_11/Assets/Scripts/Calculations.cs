7d8392c847c572bae0523adde0a3ce7475b95504
Kyler Draper <kythegamedeveloper@gmail.com>
2018-02-12 13:41:07 -0700

    Added Lab_11 Hypotenuse Calculator

---
+++ Lab_11/Assets/Scripts/Calculations.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class Calculations : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//Create function calHypo()
+	//Name: CalcHype
+	//Purpose: Calculate the Hypotenuse
+	//Inputs: SideA and SideB
+	//Outputs: Update txtHyp with Hypotenuse
+	//Preconditions: SideA and sideB are positive numbers
+	//Postconditions: The output has worked
+}
+
