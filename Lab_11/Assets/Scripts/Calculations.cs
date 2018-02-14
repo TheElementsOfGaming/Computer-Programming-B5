76669fd924d8002b13ba30d296f3a9cb18b43646
Kyler Draper <kythegamedeveloper@gmail.com>
2018-02-14 14:21:44 -0700

    Finished Lab_11 Hypotenuse Calculator

--- Lab_11/Assets/Scripts/Calculations.cs
+++ Lab_11/Assets/Scripts/Calculations.cs
@@ -1,25 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+using UnityEngine.UI;
+using System;
 
 public class Calculations : MonoBehaviour {
+	public InputField txtsideA, txtsideB;
+	public Text txtHypo, txtArea;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	//Create function calHypo()
 	//Name: CalcHype
 	//Purpose: Calculate the Hypotenuse
 	//Inputs: SideA and SideB
 	//Outputs: Update txtHyp with Hypotenuse
 	//Preconditions: SideA and sideB are positive numbers
 	//Postconditions: The output has worked
+	public void CalcHype() {
+		double SideA, sideB, Hypotenuse;
+		double.TryParse(txtsideA.text, out SideA);
+		double.TryParse(txtsideB.text, out sideB);
+
+		Hypotenuse = Math.Sqrt(Math.Pow(SideA,2) + Math.Pow(sideB,2));
+		
+		txtHypo.text = Hypotenuse.ToString();
+
+	}
+	public void CalcArea() {
+		double sideA, sideB, Area;
+		double.TryParse(txtsideA.text, out sideA);
+		double.TryParse(txtsideB.text, out sideB);
+
+		Area = (sideA * sideB)/2;
+		txtArea.text = Area.ToString();
+	}
 }
 
