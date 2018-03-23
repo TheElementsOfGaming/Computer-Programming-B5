9b68f42c89b31b424871e5c7f7919ebc7418f5c1
Kyler Draper <kythegamedeveloper@gmail.com>
2018-03-23 14:18:20 -0600

    Updated RaceCars to v1.1.2

--- RaceCars/RaceCars/Program.cs
+++ RaceCars/RaceCars/Program.cs
@@ -2,9 +2,10 @@
 
 namespace RaceCars
 {
-    class Car
+    public class Car
     {
         //properties
+        public string Name { get; set;  }
         public float Speed { get; set; }
         public float Fuel { get; set; }
         public string Make { get; set; }
@@ -17,9 +18,10 @@ namespace RaceCars
         //constructor
         public Car()
         {
+            Name = "Finn";
             Location = 0;
             Fuel = 22;
-            Speed = 210;
+            Speed = 105;
             Make = "Chevy";
             Model = "Corvette ZR1";
             Color = "Electic Blue";
@@ -27,8 +29,9 @@ namespace RaceCars
             PixelsPerGallon = 50;
         }
         public Car(float _location, float _speed, float _fuel, string _make, string _model, 
-                   int _year, float _ppg, string _color)
+                   int _year, float _ppg, string _color, string _name)
         {
+            Name = _name;
             Location = _location;
             PixelsPerGallon = _ppg;
             Speed = _speed;
@@ -59,8 +62,8 @@ namespace RaceCars
                 }
                 Location += Speed;
                 Fuel -= Speed / PixelsPerGallon;
-                Console.WriteLine("Your car is {0} pixels from the start!", Location);
-                Console.WriteLine("You  have {0} gallons of fuel remaining!", Fuel);
+                Console.WriteLine("{0} is {1} pixels from the start!", Name, Location);
+                Console.WriteLine("{0} has {1} gallons of fuel remaining!", Name, Fuel);
             }
             else 
             {
@@ -77,9 +80,9 @@ namespace RaceCars
             const int RACE_LENGTH = 500;
 
             Car Phil = new Car();
-            Car Luke = new Car(0, 200, 21, "Dodge", "Viper", 2017, 50, "Miami Blue");
-            Car Luigi = new Car(0, 2, 25, "Ford", "Pinto", 1981, 40, "Green");
-            Car Mario = new Car(0, 125, 21, "Dodge", "Challenger", 2015, 20, "Red");
+            Car Luke = new Car(0, 100, 21, "Dodge", "Viper", 2017, 50, "Miami Blue", "Mickey");
+            Car Luigi = new Car(0, 75, 25, "Ford", "GT", 1981, 40, "Green", "Melody");
+            Car Mario = new Car(0, 65, 21, "Dodge", "Challenger", 2015, 20, "Red", "Marilyn");
             //create an array of cars
             Car[] cars = new Car[4];
             //Add cars to the array
@@ -94,16 +97,23 @@ namespace RaceCars
             }
             //create a random number generator
             Random randy = new Random();
+            bool isWinner = false;
 
-            //race each car
-            foreach (Car theCar in cars)
+            while (!isWinner)
             {
-                if (theCar.Location < RACE_LENGTH && theCar.Fuel > 0)
+                //race each car
+                foreach (Car theCar in cars)
                 {
-                    theCar.Drive(randy.Next(0, 101));
+                    if (theCar.Location < RACE_LENGTH && theCar.Fuel > 0)
+                    {
+                        theCar.Drive(randy.Next(0, 50));
 
+                    }
+                    if (theCar.Location >= RACE_LENGTH) {
+                        isWinner = true;
+                    }
                 }
             }
-       }
-    }
+        }
+     }
 }
