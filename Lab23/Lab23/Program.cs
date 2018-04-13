0ff4ef39a58eacf594c151b880ea6ce701b91e22
Kyler Draper <32275825+TheElementsOfGaming@users.noreply.github.com>
2018-04-13 14:20:03 -0600

    Added Lab_23

---
+++ Lab23/Lab23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab23
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Print();
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Kyler Draper");
+            Console.WriteLine("Computer Programming B5");
+            Console.WriteLine("Lab 23");
+            Console.WriteLine("================================================");
+            Console.Title = "Lab 23 Passing by Value and Passing by Reference © Kyler Draper";
+        }
+    }
+}
